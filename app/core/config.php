d2594ee1deb5c40540aa90db3621e98ee72d3d96
Rashmi Pathinayaka <140165202+rashmipathinayaka@users.noreply.github.com>
2025-02-06 23:46:49 +0530

    added mvc files

---
+++ app/core/config.php
@@ -0,0 +1,33 @@
+<?php 
+
+if($_SERVER['SERVER_NAME'] == 'localhost')
+{
+	/** database config **/
+	define('DBNAME', 'green_lease');
+	define('DBHOST', 'localhost');
+	define('DBUSER', 'root');
+	define('DBPASS', '');
+	define('DBDRIVER', '');
+
+	define('ROOT', dirname(dirname(__FILE__))); 
+	define('URLROOT', 'http://localhost/gl2/public');
+	
+
+}else
+{
+	/** database config **/
+	define('DBNAME', 'abc');
+	define('DBHOST', 'localhost');
+	define('DBUSER', 'root');
+	define('DBPASS', '');
+	define('DBDRIVER', '');
+
+	define('ROOT', 'https://www.yourwebsite.com');
+
+}
+
+define('APP_NAME', "My Webiste");
+define('APP_DESC', "Best website on the planet");
+
+/** true means show errors **/
+define('DEBUG', true);
